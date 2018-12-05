2309e9df19e339155ac5b127aed7ffa2ecf04e4b
Jenkins Automation Server <jenkins@chef.localdomain>
2018-12-05 18:22:08 +0000

    Switch cookbook to version 1.0.11

--- cookbooks/task10/attributes/default.rb
+++ cookbooks/task10/attributes/default.rb
@@ -3,4 +3,4 @@ node.default['service_name']='task10'
 node.default['service_host']='localhost'
 node.default['service_port1']=8080
 node.default['service_port2']=8081
-node.default['version']='1.0.10'
+node.default['version']='1.0.11'
